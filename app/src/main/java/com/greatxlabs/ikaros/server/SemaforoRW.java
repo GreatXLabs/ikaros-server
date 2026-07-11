a1d4f714041e07e2b480413cc66ca4d7bbf4c191
ECubeCode <monteroemiliano.b@gmail.com>
2026-07-11 18:38:51 -0300

    fix: replace SemaforoRW with ReentrantReadWriteLock and add socket timeout
    - Replace custom 3-semaphore readers-writers implementation with
      java.util.concurrent.locks.ReentrantReadWriteLock for reliability
    - Add 120s socket timeout to ManejadorCliente to prevent thread hangs
      on disconnected clients

--- app/src/main/java/com/greatxlabs/ikaros/server/SemaforoRW.java
+++ app/src/main/java/com/greatxlabs/ikaros/server/SemaforoRW.java
@@ -1,53 +1,24 @@
 package com.greatxlabs.ikaros.server;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class SemaforoRW {
 
-    private final Semaphore mutex = new Semaphore(1);
-    private final Semaphore recurso = new Semaphore(1, true);
-    private final Semaphore turno = new Semaphore(1, true);
-    private int lectoresActivos = 0;
+    private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock(true);
 
     public void iniciarLectura() throws InterruptedException {
-        turno.acquire();
-        mutex.acquire();
-        lectoresActivos++;
-        if (lectoresActivos == 1) {
-            try {
-                recurso.acquire();
-            } catch (InterruptedException e) {
-                lectoresActivos--;
-                mutex.release();
-                turno.release();
-                throw e;
-            }
-        }
-        mutex.release();
-        turno.release();
+        rwLock.readLock().lockInterruptibly();
     }
 
     public void terminarLectura() throws InterruptedException {
-        mutex.acquire();
-        lectoresActivos--;
-        if (lectoresActivos == 0) {
-            recurso.release();
-        }
-        mutex.release();
+        rwLock.readLock().unlock();
     }
 
     public void iniciarEscritura() throws InterruptedException {
-        turno.acquire();
-        try {
-            recurso.acquire();
-        } catch (InterruptedException e) {
-            turno.release();
-            throw e;
-        }
+        rwLock.writeLock().lockInterruptibly();
     }
 
     public void terminarEscritura() {
-        recurso.release();
-        turno.release();
+        rwLock.writeLock().unlock();
     }
 }
