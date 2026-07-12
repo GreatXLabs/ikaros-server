4fd93b075244d3279932045d1b3b5d902add6dad
ECubeCode <monteroemiliano.b@gmail.com>
2026-07-12 00:23:26 -0300

    refactor(server): restore 3-semaphore SemaforoRW with deadlock fix

--- app/src/main/java/com/greatxlabs/ikaros/server/SemaforoRW.java
+++ app/src/main/java/com/greatxlabs/ikaros/server/SemaforoRW.java
@@ -1,24 +1,53 @@
 package com.greatxlabs.ikaros.server;
 
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.Semaphore;
 
 public class SemaforoRW {
 
-    private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock(true);
+    private final Semaphore mutex = new Semaphore(1);
+    private final Semaphore recurso = new Semaphore(1, true);
+    private final Semaphore turno = new Semaphore(1, true);
+    private int lectoresActivos = 0;
 
     public void iniciarLectura() throws InterruptedException {
-        rwLock.readLock().lockInterruptibly();
+        turno.acquire();
+        mutex.acquire();
+        lectoresActivos++;
+        if (lectoresActivos == 1) {
+            try {
+                recurso.acquire();
+            } catch (InterruptedException e) {
+                lectoresActivos--;
+                mutex.release();
+                turno.release();
+                throw e;
+            }
+        }
+        mutex.release();
+        turno.release();
     }
 
     public void terminarLectura() throws InterruptedException {
-        rwLock.readLock().unlock();
+        mutex.acquire();
+        lectoresActivos--;
+        if (lectoresActivos == 0) {
+            recurso.release();
+        }
+        mutex.release();
     }
 
     public void iniciarEscritura() throws InterruptedException {
-        rwLock.writeLock().lockInterruptibly();
+        turno.acquire();
+        try {
+            recurso.acquire();
+        } catch (InterruptedException e) {
+            turno.release();
+            throw e;
+        }
     }
 
     public void terminarEscritura() {
-        rwLock.writeLock().unlock();
+        recurso.release();
+        turno.release();
     }
 }
